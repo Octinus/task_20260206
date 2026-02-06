a6451a4e9536a3e5ae84b110c4a426af1cf32caf
Octinus <oct.23rd.c@gmail.com>
2026-02-07 05:19:08 +0900

    등록 파라미터에 파일만 있는 경우 파일이름과 이름으로 확장자까지 확인. csv,json이 아니면 예외처리

---
+++ EmergencyContactApi/Helpers/ImportRequestParser.cs
@@ -0,0 +1,31 @@
+﻿namespace EmergencyContactApi.Helpers
+{
+    public static class ImportRequestParser
+    {
+        public static string GetFileName(IFormFile file)
+        {
+            return file.FileName;
+        }
+
+        public static string CheckFileFormat(string? fileName)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(fileName))
+                    throw new NullReferenceException("파일명이 없습니다.");
+
+                    string extension = Path.GetExtension(fileName);
+                    if (string.Equals(extension, ".json", StringComparison.OrdinalIgnoreCase))
+                        return "json";
+                    if (string.Equals(extension, ".csv", StringComparison.OrdinalIgnoreCase))
+                        return "csv";
+                throw new ArgumentException("지원하지 않는 확장자의 파일입니다.");
+            }
+            catch
+            {
+                throw;
+            }
+                
+        }
+    }
+}
--- EmergencyContactApi/Services/ServiceImpls/Employees/RegisterImpl.cs
+++ EmergencyContactApi/Services/ServiceImpls/Employees/RegisterImpl.cs
@@ -1,4 +1,5 @@
 ﻿using EmergencyContactApi.DataStorages.Interfaces;
+using EmergencyContactApi.Helpers;
 using EmergencyContactApi.Models.Commons;
 using EmergencyContactApi.Models.Request;
 using EmergencyContactApi.Models.Results;
@@ -19,31 +20,38 @@ namespace EmergencyContactApi.Services.ServiceImpls.Employees
         {
             bool hasFile = request.FormFile is not null;
             bool hasRawString = request.RawString is not null;
-            if(hasFile && hasRawString)
+            try
+            {
+                if (hasFile && hasRawString)
+                {
+                    throw new Exception("파일업로드, 직접입력 둘 중 하나만 입력해주세요.");
+                }
+
+                if (!hasFile && !hasRawString)
+                {
+                    throw new Exception("파일업로드, 직접입력 아무것도 없습니다.");
+                }
+
+                if (hasFile)
+                {
+                    string fileName = ImportRequestParser.GetFileName(request.FormFile);
+                    string fileExtsion = ImportRequestParser.CheckFileFormat(fileName);
+                    return null;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
             {
                 return new ApiResponse<RegisterResult>
                 {
                     Success = false,
                     Result = null,
-                    Error = "파일업로드, 직접입력 둘중 하나만 입력해주세요."
+                    Error = ex.Message
                 };
-            }
-            else if(hasFile && !hasRawString)
-            {
-                return null;
-            }
-            else if(hasRawString && !hasFile)
-            {
-                return null;
-            }
-            else
-            {
-                return new ApiResponse<RegisterResult>
-                {
-                    Success = false,
-                    Result = null,
-                    Error = "File, RawString 아무것도 없습니다."
-                };
+
             }
         }
     }
