cba301c5abab7a03d5297cb266b6c260d617bae1
Octinus <oct.23rd.c@gmail.com>
2026-02-07 02:44:49 +0900

    feat: DB 및 저장 프로시저 역할을 대체하는 DataStorage 구현 1차 완료.

--- EmergencyContactApi/Controllers/Employee/RegisterController.cs
+++ EmergencyContactApi/Controllers/Employee/RegisterController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EmergencyContactApi.Controllers.Employee
 {
+    /// <summary>
+    /// 직원 등록 관련 API를 제공하는 컨트롤러.
+    /// </summary>
     [Route("api/employee")]
     [ApiController]
     [Tags("Employee")]
