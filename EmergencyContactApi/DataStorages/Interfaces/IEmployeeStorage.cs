90f327451bafccc1abe0d1ce2d16c5f2a6b33856
Octinus <oct.23rd.c@gmail.com>
2026-02-07 02:49:34 +0900

    Reapply "feat: DB 및 저장 프로시저 역할을 대체하는 DataStorage 구현 1차 완료."
    This reverts commit e82495107eeec4e10859c77eb2817efa15d4b355.

---
+++ EmergencyContactApi/DataStorages/Interfaces/IEmployeeStorage.cs
@@ -0,0 +1,11 @@
+﻿using EmergencyContactApi.Models.Entity;
+
+namespace EmergencyContactApi.DataStorages.Interfaces
+{
+    public interface IEmployeeStorage
+    {
+        List<Employee> GetEmployeePagedList(int page, int pageSize);
+        Employee? GetEmployeeInformationByName(string name);
+        void AddEmployees(List<Employee> employees);
+    }
+}
